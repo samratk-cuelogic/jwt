1a4dd2c61cfc2381e9ab69a7ed0977204f2969d1
samratk-cuelogic <samrat.kasture@cuelogic.com>
2018-04-12 10:20:42 +0530

    JWT commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    'secret': 'secretKey',
+    'database': 'mongodb://localhost:27017/dbemployee'
+    //'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/employeedb'
+
+};
